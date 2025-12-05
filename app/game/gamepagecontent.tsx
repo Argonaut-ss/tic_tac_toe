6441be8d32c0cbf9ccb6bc8fe53a3bd674e50309
sam <81003477+Argonaut-ss@users.noreply.github.com>
2025-12-05 17:02:13 +0700

    update

---
+++ app/game/gamepagecontent.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import GameBoardUI from "../components/gameboard/gameboardUI";
+import { useTicTacToe } from "../hooks/useTicTacToe";
+
+export default function GamePageContent() {
+  const params = useSearchParams();
+  const mark = (params.get("mark") as "X" | "O") || "X";
+
+  const { board, turn, winner, score, playerMove, resetBoard } = useTicTacToe(mark);
+  const requestRematch = () => resetBoard(false);
+
+  return (
+    <GameBoardUI
+      board={board}
+      turn={turn}
+      winner={winner}
+      score={score}
+      playerMark={mark}
+      botMark={mark === "X" ? "O" : "X"}
+      playerMove={playerMove}
+      resetBoard={resetBoard}
+      requestRematch={requestRematch}
+    />
+  );
+}
